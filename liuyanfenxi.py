a7b135e1d8b2f0fd89f00bdd2251b49cca8c67e9
StellaShi <stella@apples-MacBook-Pro.local>
2019-12-20 14:32:35 +0200

    caculate avg lenth of reviews

--- liuyanfenxi.py
+++ liuyanfenxi.py
@@ -4,12 +4,14 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # 等于 count = count +1
-		if count % 1000 == 0: # count 除以 1000的余数是多少
+		if count % 10000  == 0: # count 除以 1000的余数是多少
 			print (len(data))
 
-print(len(data))
+print('档案读取完了，总共有', len(data), '笔资料')
 
-print(data[0])
-print(data[230]) 
-print('--------------------')#分隔符
-print(data[93])
+sum_len = 0
+for d in data: #每笔资料命名为d，每个d是一个字串
+	sum_len += len(d) #每笔留言的长度和现在的留言长度加在一起，然后存入目前的总数
+
+print('留言的平均长度为', sum_len/len(data), '个字') #总长度除以数据条数
+
